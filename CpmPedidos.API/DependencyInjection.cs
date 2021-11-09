82ae59bb2216001d4961ba04596abf1e6524363c
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2021-11-08 21:12:03 -0300

    Add project files.

---
+++ CpmPedidos.API/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+
+namespace CpmPedidos.API
+{
+    public class DependencyInjection
+    {
+        public static void Register(IServiceCollection serviceProvider) 
+        {
+           // RepositoryDependece(serviceProvider);
+            RepositoryDependence(serviceProvider);
+        }
+
+        private static  void  RepositoryDependence(IServiceCollection serviceProvider)
+        {
+        }
+    }
+}
